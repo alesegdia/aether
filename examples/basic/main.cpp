082375fcf7c72339af6fa4e6694b28a067fb68fb
Alejandro Seguí <alesegdia@gmail.com>
2018-04-20 02:48:47 +0200

    adding working examples

---
+++ examples/basic/main.cpp
@@ -0,0 +1,12 @@
+#include "../src/core/application.h"
+
+class MyGame : public aether::core::Application
+{
+public:
+    MyGame(int w, int h) : aether::core::Application(w, h) {}
+};
+
+int main( int argc, char** argv )
+{
+    MyGame(200, 200).exec(argc, argv);
+}
---
+++ examples/texture/main.cpp
@@ -0,0 +1,31 @@
+#include "../src/core/application.h"
+#include "../src/graphics/texture.h"
+
+
+class MyGame : public aether::core::Application
+{
+public:
+    MyGame(int w, int h) : aether::core::Application(w, h) {}
+
+    int ready(int argc, char **argv) override
+    {
+        m_texture.load("media/aether-logo.png");
+        return 0;
+    }
+
+    void render() override
+    {
+        aether::core::Application::render();
+        m_texture.draw(10, 10);
+    }
+
+private:
+    aether::graphics::Texture m_texture;
+
+
+};
+
+int main( int argc, char** argv )
+{
+    MyGame(200, 200).exec(argc, argv);
+}
