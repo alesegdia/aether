9bc5cb23ebb18cc1e72cf4b649d55b7021297033
Alejandro Seguí <alesegdia@gmail.com>
2018-04-23 18:18:01 +0200

    adding posibility of drawing a circle with alpha; this must be changed

--- src/aether/graphics/graphics.h
+++ src/aether/graphics/graphics.h
@@ -9,6 +9,7 @@ void clear(float r, float g, float b);
 void draw_rectangle(float x1, float y1, float x2, float y2, Color c, float thickness = 1.0f);
 void draw_filled_rectangle(float x1, float y1, float x2, float y2, Color c);
 void draw_filled_circle(float x, float y, float radius, Color c);
+void draw_filled_circle_alpha(float x, float y, float radius, Color c, int alpha);
 
 }
 }
