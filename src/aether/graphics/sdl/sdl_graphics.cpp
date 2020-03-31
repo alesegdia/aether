f6abd1dbfef88d4b8a1c621839aa9b7a4788a8a1
Alejandro Seguí <alesegdia@gmail.com>
2020-03-31 21:49:35 +0200

    adding drawing primitives code for sdl

--- src/aether/graphics/sdl/sdl_graphics.cpp
+++ src/aether/graphics/sdl/sdl_graphics.cpp
@@ -20,17 +20,22 @@ void draw_rectangle(float x1, float y1, float x2, float y2, Color c, float thick
 
 void draw_filled_rectangle(float x1, float y1, float x2, float y2, Color c)
 {
+    math::Recti rect = { int(x1), int(y1), int(x2 - x1), int(y2 - y2) };
+    draw_filled_rectangle(rect, c);
+}
 
+void draw_filled_rectangle(const math::Recti& rect, Color c)
+{
+    SDL_SetRenderDrawColor(aether_sdl_get_renderer(), c.r, c.g, c.b, 255);
+    SDL_Rect sdlrect = { rect.x(), rect.y(), rect.w(), rect.h() };
+    SDL_RenderFillRect(aether_sdl_get_renderer(), &sdlrect);
 }
 
 void draw_filled_rectangle(const math::Rectf& r, Color c)
 {
-
-}
-
-void draw_filled_rectangle(const math::Recti& r, Color c)
-{
-
+    SDL_SetRenderDrawColor(aether_sdl_get_renderer(), c.r, c.g, c.b, 255);
+    SDL_Rect sdlrect = { r.x(), r.y(), r.w(), r.h() };
+    SDL_RenderFillRect(aether_sdl_get_renderer(), &sdlrect);
 }
 
 void draw_filled_rectangle_centered(const math::Recti& r, Color c)
