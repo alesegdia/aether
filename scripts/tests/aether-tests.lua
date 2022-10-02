cf44f4d75b5ee5a4de1296a573bc144ec281ecea
Alejandro Seguí <alesegdia@gmail.com>
2022-10-02 17:03:52 +0200

    updating tests

---
+++ scripts/tests/aether-tests.lua
@@ -0,0 +1,12 @@
+aetherProject("aether-tests")
+	includedirs {
+		path.join(AETHER_DIR, "module/googletest/googletest/include"),
+		path.join(AETHER_DIR, "module/googletest/googlemock/include"),
+	}
+	debugdir ("..")
+	targetdir ("../build")
+	links { "gtest" }
+	files {
+    	path.join(AETHER_DIR, "src/tests/maintests.cpp"),
+    	path.join(AETHER_DIR, "module/secs/src/gtests/**.cpp"),
+	}
